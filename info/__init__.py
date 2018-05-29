41c481a118ab425a3058b0465eeb11f18b4c7dda
Demoyuge <18556579095@163.com>
2018-05-29 11:30:05 +0800

    config

--- info/__init__.py
+++ info/__init__.py
@@ -42,7 +42,7 @@ def create_app(config_name):
     db.init_app(app)
     #创建连接到redis 数据库的对象
     global redis_store
-    redis_store = StrictRedis(host=configs[config_name].REDIS_HOST,port=configs[config_name].REDIS_PORT)
+    redis_store = StrictRedis(host=configs[config_name].REDIS_HOST,port=configs[config_name].REDIS_PORT,decode_responses=True)
 
     #开启CSRF保护
     CSRFProtect(app)
