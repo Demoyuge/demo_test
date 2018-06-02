56f6ea1dc65be9d544467ee31d7b00e66ad7dcf5
Demoyuge <18556579095@163.com>
2018-06-02 23:32:58 +0800

    收藏逻辑

--- info/modules/news/views.py
+++ info/modules/news/views.py
@@ -1,10 +1,65 @@
 #新闻详情：收藏，评论点赞
+from info.response_code import RET
 from info.utils.comment import user_login_data
 from . import news_blue
 from flask import render_template, session, current_app, abort, g, jsonify, request
-from info.models import User,News
+from info.models import User, News, Comment
 from info import  constants,db,response_code
 from info.utils.comment import  user_login_data
+
+
+
+@news_blue.route('/news_comment',methods=['POST'])
+@user_login_data
+def add_news_comment():
+
+    user = g.user
+    if not user:
+        return jsonify(errno = RET.SESSIONERR, errmsg="用户未登录")
+
+
+    # 2.获取请求参数
+    news_id = request.json.get('news_id')
+    comment_content = request.json.get('comment')
+    # 没有parent_id表示评论信息；反之，是回复评论
+    parent_id = request.json.get('parent_id')
+
+    if not all([news_id,comment_content]):
+        return jsonify(errno = RET.PARAMERR, errmsg = "参数不足")
+    try:
+        news_id = int(news_id)
+        if parent_id:
+            parent_id = int(parent_id)
+    except Exception as e:
+        current_app.logger.error(e)
+        return jsonify(errno=response_code.RET.PARAMERR, errmsg='缺少参数')
+
+    try:
+        news = News.query.get(news_id)
+    except Exception as e :
+        current_app.logger.error(e)
+        return jsonify(errno = RET.DBERR, errmsg="查询数据失败")
+
+    if not news:
+        return  jsonify(errno=RET.NODATA,errmsg= "该新闻不存在")
+
+    comment =  Comment()
+    comment.user_id = user.id
+    comment.news_id  = news_id
+    comment.content = comment_content
+    if parent_id:
+        comment.parent_id = parent_id
+
+    try:
+        db.session.add(comment)
+        db.session.commit()
+    except Exception as e:
+        db.session.rollback()
+        current_app.logger.error(e)
+        return  jsonify(errno = RET.DBERR, errmsg = "保存评论数据失败")
+
+    return  jsonify(errno = RET.OK, errmsg = "评论成功",data=comment.to_dict())
+
 
 
 @news_blue.route('/news_collect',methods=['POST'])
@@ -69,7 +124,7 @@ def news_detail(news_id):
     # 使用函数封装获取用户信息
     user = g.user
     # 2.新闻的点击排行
-    news_clicks = []
+    news_clicks = None
     try:
         news_clicks = News.query.order_by(News.clicks.desc()).limit(constants.CLICK_RANK_MAX_NEWS)
     except Exception as e:
@@ -96,10 +151,23 @@ def news_detail(news_id):
     if user:
         if news in user.collection_news:
             is_collected = True
+    comments = []
+    try:
+        comments = Comment.query.filter(Comment.news_id == news_id).order_by(Comment.create_time.desc()).all()
+    except Exception as e:
+        current_app.logger.error(e)
+    comments_dict_list = []
+    for comment in comments:
+
+        comments_dict_list.append(comment.to_dict())
+
+
+
     context = {
         'user':user,
         'news_clicks':news_clicks,
         'news':news.to_dict(),
-        'is_collected':is_collected
+        'is_collected':is_collected,
+        'comments':comments_dict_list 
     }
     return  render_template('news/detail.html',context =context )
